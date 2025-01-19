e03eba5cc74a365a88fcd54725750ab590d39bc6
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-01-19 03:03:21 +0000

    Jan 19, 2025, 3:03 AM

--- CT07_01/lesson1.py
+++ CT07_01/lesson1.py
@@ -1,2 +1,58 @@
-print("Hello from lesson 1")
+# print("Rawr")
+
+# #Get user's name#
+
+
+# num1 = input("Enter a number!")
+# num2 = input("Enter another number!")
+            
+
+# ans = num1 + num2 
+# print (ans)
+
+#start
+# Create "plastic" bin
+# Create "paper" bin
+# Create "glass" bin
+
+#Check if user input is glass
+# If it is glass,
+# Say "This goes in the glass bin"
+ # Put into "glass" bin
+
+#  Check if user input is plastic
+# If it is plastic,
+# Say "This goes in the plastic bin"
+#Put into "plastic" bin
+
+# Check if user input is paper
+# If it is paper,
+# Say "This goes in the paper bin"
+# Put into "paper" bin
+
+
+
+
+
+
+
+
+
+
+# red_plate = 1
+# blue_plate = 2 
+# green_plate = 3 
+
+# red = input("How many red plates ordered?")
+
+
+
+# blue = input("How many blue plates ordered?")
+
+
+# green = input("How many green plates ordered?")
+
+# cost = (red_plate * red) + (blue_plate * blue) + (green_plate * green)
+
+# print (cost)
 
