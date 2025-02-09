f9007ca8e4926254f0cbc1ea8f196ad71fc1cf79
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-02-09 03:24:29 +0000

    Feb 9, 2025, 3:24 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -17,11 +17,13 @@ num = random.randint(1 , 20)
 total_Q = 15
 lives = 3 
 for i in range(total_Q):
-    num = random.randint(2,20)
+    num1 = random.randint(2,20)
     num2 = random.randint(2,20)
+    correct = num1 * num2
 
     while lives>0:
         answer = int(input("What's {num1} x {num2}"))
 
-        
+        if answer == correct:
+            
    
