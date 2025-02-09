fccf05572b4c2123130674d4033067a83233eaea
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-02-09 03:14:10 +0000

    Feb 9, 2025, 3:14 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -9,12 +9,4 @@
 
 # print("Completed ")
 
-savings = 0
-
-while savings < 100:
-   daily_saving = float(input("How much did you save today?"))
-   savings += daily_saving
-
-   if savings >= 100:
-      print("AY LET'S GO BRUH! YOU'VE SAVED $100 AND CAN AFFORD CHILD SUPPORT!")
-      break
+#  im
