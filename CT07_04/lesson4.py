6fca1a78e442ed6b7df22a3c5753a78df113d58b
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-02-09 04:11:43 +0000

    Feb 9, 2025, 4:11 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -1,25 +1,27 @@
 print("Hello from lesson 4")
 
-Planets = ["Mercury" , "Venus" , "Earth" , "Mars" , "Jupiter" , "Saturn" , "Uranus" , "Neptune"]
+# Planets = ["Mercury" , "Venus" , "Earth" , "Mars" , "Jupiter" , "Saturn" , "Uranus" , "Neptune"]
 
 
-print(Planets[3])
-Planets[3] = "Gyat Prime"
-print(Planets[3])
-print(len(Planets))
-for counter in range(len(Planets)):
-    print(Planets[counter])
+# print(Planets[3])
+# Planets[3] = "Gyat Prime"
+# print(Planets[3])
+# print(len(Planets))
+# for counter in range(len(Planets)):
+#     print(Planets[counter])
 
-Planets.insert(3, "Jawline Land")
-Planets.append("Blutoes")
+# Planets.insert(3, "Jawline Land")
+# Planets.append("Blutoes")
 
 
-for counter in range(len(Planets)):
-    print(Planets[counter])
+# for counter in range(len(Planets)):
+#     print(Planets[counter])
      
-    if Planets[counter] == "Earth":
-        print("This is home, truly,where I know I must be...")
-    if Planets[counter] == "Gyat Prime":
-        print("GYATTTTTTT!!!!")
-    if Planets[counter] == "Jawline Land":
-        print("Sigma")
+#     if Planets[counter] == "Earth":
+#         print("This is home, truly,where I know I must be...")
+#     if Planets[counter] == "Gyat Prime":
+#         print("GYATTTTTTT!!!!")
+#     if Planets[counter] == "Jawline Land":
+#         print("Sigma")
+
+
