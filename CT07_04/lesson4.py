4406d25db19a8e1b44d345353bc6aae6a00c699e
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-02-09 03:52:02 +0000

    Feb 9, 2025, 3:52 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -3,7 +3,7 @@ print("Hello from lesson 4")
 Planets = ["Mercury" , "Venus" , "Earth" , "Mars" , "Jupiter" , "Saturn" , "Uranus" , "Neptune"]
 
 
-prints(Planets[3])
+print(Planets[3])
 Planets[3] = "Gyat Prime"
 print(Planets[3])
 print(len(Plants))
