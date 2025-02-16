2b22b829d6279fb2d5845c239904e1b0a923c0fb
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-02-16 03:46:33 +0000

    Feb 16, 2025, 3:46 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -1,3 +1,23 @@
 print("Hello from lesson 5")
 
+import random
 
+counter = 0 
+
+lucky_winners = []
+
+while True:
+
+    num = random.randint(1,1000)
+
+    lucky_winners.append(num)
+
+    for num in range(100):
+
+        print(num)
+
+    counter += 1
+
+    if counter >100:
+
+        break
