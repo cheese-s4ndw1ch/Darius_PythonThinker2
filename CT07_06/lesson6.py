a7b1e9f8173201c517cbf00257b82413c177b70b
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-02-23 03:31:37 +0000

    Feb 23, 2025, 3:31 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -4,6 +4,8 @@ contacts = []
 contact1 = ["John", 98453126, "john@gmail.com"]
 contact2 = ["Adam", 93029102, "adam@gmail.com"]
 contact3 = ["Sylvia", 87894032, "sylvia@gmail.com"]
+
+
  
 for contact1 in contacts:
     for contact2 in contact1:
