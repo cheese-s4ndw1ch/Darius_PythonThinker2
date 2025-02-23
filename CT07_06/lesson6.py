1cd38d4afba62c34b36b7328b898925f29d88e5c
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-02-23 03:43:13 +0000

    Feb 23, 2025, 3:43 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -1,17 +1,17 @@
-print("Hello from lesson 6")
+# print("Hello from lesson 6")
 
-contacts = []
-contact1 = ["John", 98453126, "john@gmail.com"]
-contact2 = ["Adam", 93029102, "adam@gmail.com"]
-contact3 = ["Sylvia", 87894032, "sylvia@gmail.com"]
+# contacts = []
+# contact1 = ["John", 98453126, "john@gmail.com"]
+# contact2 = ["Adam", 93029102, "adam@gmail.com"]
+# contact3 = ["Sylvia", 87894032, "sylvia@gmail.com"]
 
-contacts.append(contact1)
-contacts.append(contact2)
-contacts.append(contact3)
+# contacts.append(contact1)
+# contacts.append(contact2)
+# contacts.append(contact3)
  
-for contact in contacts:
-    for details in contacts:
-        print(details)
+# for contact in contacts:
+#     for details in contacts:
+#         print(details)
 
 
 students = [
