8bcf7b53e55098337e647f0825509143dd7b3ae2
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-02-23 03:20:18 +0000

    Feb 23, 2025, 3:20 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -1,2 +1,6 @@
 print("Hello from lesson 6")
 
+contacts = []
+contact1 = ["John", 98453126, "john@gmail.com"]
+contact2 = ["Adam", 93029102, "adam@gmail.com"]
+contact3 = ["Sylvia", 87894032, "sylvia@gmail.com"]
