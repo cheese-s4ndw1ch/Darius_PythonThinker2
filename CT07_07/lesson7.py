4123114abcc956e3f0ddc2c5dcc70553c8761eb9
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-03-02 04:16:33 +0000

    Mar 2, 2025, 4:16 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -46,9 +46,9 @@ print("Hello from lesson 7")
 #list1 = ['Apple', 'Banana', 'Cherry', 'Durian']
 #list2 = ['Cherry' , 'Durian', 'Elderberry', 'Figs']
 
-unique = []
+#unique = []
 
-element = 3
+#element = 3
 
 
 list3 = list1 + list2
