18c9dc99dcfc83c02d01e32b1593c43875f311d9
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-03-02 03:25:26 +0000

    Mar 2, 2025, 3:25 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -30,7 +30,7 @@ print("Hello from lesson 7")
 
 #fruits = ["Apple", "Banana", "Cherry", "Durian", "Elderberry", "Figs"]
 #index = 3
-left = fruits[:index]
+#left = fruits[:index]
 right = fruits[index:]
 
 print(left)
