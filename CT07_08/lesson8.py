cad9bf9ac45adb672ced82d4a1e86567b40dae6d
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-03-16 03:51:15 +0000

    Mar 16, 2025, 3:51 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -13,26 +13,31 @@
 # print(left)
 
 
-pw = input("Please create password (Must be more than 8 letters, 1 uppercase + 1 lower case, must have a number): ")
+# pw = input("Please create password (Must be more than 8 letters, 1 uppercase + 1 lower case, must have a number): ")
 
-is_8char_long = True
-has_upper = True
-has_lower = True
-has_num = False
-only_alnum = False
+# is_8char_long = True
+# has_upper = True
+# has_lower = True
+# has_num = False
+# only_alnum = False
 
-if len(pw)>=8:
-    is_8char_long = True
-for i in pw:
-    if i.isupper()==True:
-        has_upper = True
-for i in pw:
-    if i.islower()== True:
-        has_lower = True
-for i in pw:
-    if i.isdigit()== True:
-        has_num = True
-if is_8char_long and has_upper and has_lower and has_num == True:
-    print("Password accepted.")
-else:
-    print("Password unacceptable.")
+# if len(pw)>=8:
+#     is_8char_long = True
+# for i in pw:
+#     if i.isupper()==True:
+#         has_upper = True
+# for i in pw:
+#     if i.islower()== True:
+#         has_lower = True
+# for i in pw:
+#     if i.isdigit()== True:
+#         has_num = True
+# if is_8char_long and has_upper and has_lower and has_num == True:
+#     print("Password accepted.")
+# else:
+#     print("Password unacceptable.")
+
+
+
+
+
