7e884c612f4326ef18a8ec4e59c30f2c724af3ab
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-04-06 03:02:49 +0000

    Apr 6, 2025, 3:02 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -72,6 +72,106 @@
 # print(string)
 
 
-sentence = "Adolf Hitler"
+# sentence = "Adolf Hitler"
 
-reversed_wordss = [word[]]
+# reversed_words = [word[: :-1]] for word in sentence.split()]
+# reversed_sentence = " ".join(reversed_words)
+# print(reversed_sentence)
+
+# word = "hitler"
+
+# is_palindrome = word ==word[::-1]
+# print(word[::-1])
+# print(is_palindrome)
+
+
+
+
+# while True:
+#     word = input("Type a word to check if its a palindrome: ")
+
+#     if (word == "end"):
+#         break
+
+
+
+# is_palindrome = word == word[::-1]
+# print(is_palindrome)
+
+# palindromes = 0
+# sentence = input("Provide a sentence: ")
+# list = []
+
+# word = sentence.split(",")
+# palindromes +=1
+# word.append(list)
+
+# is_palindrome = list == list[::-1]
+
+# print(is_palindrome)
+# print(palindromes)
+
+
+
+# sales_data = [["Apple", 50, 1.99], ["Banana", 45, 0.99], ["Orange", 30, 3.99], ["Grapefruit", 25, 3.99], ["Mango", 20, 2.99]]
+# earned = []
+# for fruits in sales_data:
+#     fruits, sold , price = fruits
+#     earned.append(sold * price)
+
+
+# print(earned)
+# max = 0
+# for price in earned:
+
+#     if(max < price):
+#         max = price
+
+
+# print(max)
+
+# for fruits in sales_data:
+#     fruit_max = fruits[earned.index(max)]
+
+# print(fruit_max)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# abble_msg = "abbles are gay"
+# orange_msg = "Are oranges orange because the name of the orange is orange or is it because an orange is orange because its orange"
+# banana_msg = "Banaynay"
+
+
+
+harcking101 = ["Pigga123, fivenightsatdiddy's, farteata.com"]
+
+pressings = input("Press 'a' to add, 's' to search. 'd' to display, or 'q' to quit")
+
+
+if pressings == "q":
+    break
+
+
+
+
+
+
+
+
+
+
+user = input("enter username: ")
+pw = input("Enter password: ")
+while input in user: "Pigga123"
+print("Yay correct!")
+   
--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -1,2 +1,72 @@
-print("Hello from lesson 9")
+# print("Hello from lesson 9")
+# riddle = input("Type ze riddle: ")
+
+# isCorrect = False
+# lowercase = riddle.lower()
+# riddle_split = lowercase.split()
+# for word in riddle_split:
+#     if word == "egg":
+#         isCorrect = True
+# if isCorrect: 
+#     print("Correct! Well done!")
+# else:
+#     print("Wrong! Try again nuthead!")
+
+import turtle
+
+window = turtle.Screen()
+window.setup(600,600)
+window.bgcolor("forestgreen")
+
+
+pen = turtle.Turtle()
+pen.penup()
+pen.shape("square")
+pen.sety(250)
+
+
+for i in range(-600,600,25):
+    pen.setx(i)
+    pen.stamp()
+
+
+pen.goto(-300,-250)
+pen.pencolor("yellow")
+pen.down()
+pen.seth(0)
+pen.forward(600)
+pen.hideturtle()
+
+
+gay = turtle.Turtle()
+gay.penup()
+gay.seth(90)
+gay.shape("turtle")
+gay.color("red")
+gay.goto(0, -250)
+gay.write("gay", align = "center", font=('Arial', 20))
+
+sigma = turtle.Turtle()
+sigma.penup()
+sigma.seth(90)
+sigma.shape("turtle")
+sigma.color("blue")
+sigma.goto(-200, -250)
+sigma.write("sigma", align = "center", font=('Sarpanch', 20))
+
+
+
+skibidi = turtle.Turtle()
+skibidi.penup()
+skibidi.seth(90)
+skibidi.shape("turtle")
+skibidi.color("white")
+skibidi.goto(200, -250)
+skibidi.write("skibidi", align = "center", font=('Sarpanch', 20))
+
+window.mainloop()
+
+
+ask = input("Who shall wins?")
+
 
