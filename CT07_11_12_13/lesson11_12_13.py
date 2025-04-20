1e790f6316a7c69a50d10894e5bdce0ec5a1a9a4
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-04-20 04:21:21 +0000

    Apr 20, 2025, 4:21 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -29,7 +29,7 @@ def print_board(board):
             cell_number += 1
 
         if (cell_number) <=9:
-            print("\n----------")
+            print("\n-----------")
     print("\n")
         
 
