38d7d8526017bbc51ea6e98ae50f7bead2fe363b
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-04-20 03:44:44 +0000

    Apr 20, 2025, 3:44 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -1,2 +1,14 @@
 print("Hello from lesson 11_12_13")
 
+def initialise_board():
+    board = []
+    for i in range(3):
+        row = []
+        for j in range(3):
+            row.append(' ')
+        board.append(row)
+
+    return board
+
+
+print(initialise_board()) 
