761ab1b9fa4aed4dbb7c3c98b35da1c1ff01c93b
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-04-20 04:12:05 +0000

    Apr 20, 2025, 4:12 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -23,7 +23,7 @@ def print_board():
             if cell != ' ':
                 print(" " + str(cell) + " " , end="")
             else:
-                print(" " + str)
+                print(" " + str(cell_number) )
         
 
 
