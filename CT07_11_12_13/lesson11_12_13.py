eac808458745fac2d6fd45d08a4410a9f2528ed7
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-04-27 04:20:32 +0000

    Apr 27, 2025, 4:20 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -6,7 +6,7 @@ winning_conditions = [[board[0][0]], [board[0][1]], [board[0][2]],
                       [board[1][0]], [board[1][1]], [board[1][2]],
                       [board[2][0]], [board[2][1]], [board[2][2]],
 
-                      [board[2][0]]]
+                      [board[0][0]], ]
                                                       
                         
                         
