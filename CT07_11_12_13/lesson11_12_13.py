5b867e85d7a0aa9158ffaeb3239919c7b47d9d45
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-04-27 04:16:42 +0000

    Apr 27, 2025, 4:16 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -4,7 +4,7 @@
 board = []
 winning_conditions = [[board[0][0]], [board[0][1]], [board[0][2]], 
                       [board[1][0]], [board[1][1]], [board[1][2]],
-                      [board[2][0]], [board[0][0]]]
+                      [board[2][0]], [board[2][1]]]
 def initialise_board():
     
     for i in range(3):
