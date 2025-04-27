6d0022140ac44685c537ae99ce1430f36645969d
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-04-27 04:19:31 +0000

    Apr 27, 2025, 4:19 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -4,7 +4,8 @@
 board = []
 winning_conditions = [[board[0][0]], [board[0][1]], [board[0][2]], 
                       [board[1][0]], [board[1][1]], [board[1][2]],
-                      [board[2][0]], [board[2][1]]], [board[2][2],]
+                      [board[2][0]], [board[2][1]]], [board[2][2],
+                                                      ]
                         
                         
                         
