68f78474a75a631ffce245f11da2b819d6b45216
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-04-27 03:22:03 +0000

    Apr 27, 2025, 3:22 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -34,7 +34,7 @@ def print_board(board):
         
 
 def get_player_move():
-    move_input = input("Enter your move")
+    move_input = input("Enter your move (1-9): ")
     move = int(move_input) - 1
     row = move //3
     col = move % 3
