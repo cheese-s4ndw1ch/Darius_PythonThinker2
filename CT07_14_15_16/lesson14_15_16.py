66ec26b43cc7b239f97e51145f426dbb591ea248
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-05-11 03:47:33 +0000

    May 11, 2025, 3:47 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -6,14 +6,27 @@ import pygame
 pygame.init
 
 screen_wid = 800
-screen_hei = 800
-screen = pygame.display.set_mode(screen_wid, screen_hei)
+screen_hei = 600
+
+white = (255,255,255)
+
+paddle_width = 30
+paddle_height = 100
+paddle1_x =
+paddle_y = 
 
 running = True
+
+screen = pygame.display.set_mode((screen_wid,screen_hei))
+
+pygame.display.set_caption("Pong Game")
+
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+pygame.draw.rect(screen, white, (paddle1_x, paddle_y, paddle_width, paddle_height))
 
 pygame.QUIT()
