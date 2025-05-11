2bbcfd40f4785dc10774509d0c8c8cd087c4ab90
cheese-s4ndw1ch <darius.lim.rx@gmail.com>
2025-05-11 03:32:41 +0000

    May 11, 2025, 3:32 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -1,1 +1,19 @@
 print("Hello from lesson 14_15_16")
+
+import pygame
+
+
+pygame.init
+
+screen_wid = 800
+screen_hei = 800
+screen = pygame.display.set_mode(screen_wid, screen_hei)
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.QUIT()
